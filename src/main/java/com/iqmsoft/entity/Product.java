fac5cca8be68d2df56f6d08b2f9226e7e178eac7
Muruga <davanon2014@gmail.com>
2021-05-05 11:18:03 +0530

    Up

--- src/main/java/com/iqmsoft/entity/Product.java
+++ src/main/java/com/iqmsoft/entity/Product.java
@@ -6,15 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@Getter
 @Entity
 public class Product {
     
